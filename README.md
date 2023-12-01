b004e6fcd9a1eadcc2eedce02b17db5befeb77cf
elliot b  =^._.^= ∫ <elliot@daddy.cat>
2023-12-01 10:56:15 -0500

    updated strong outputs

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Adjective Adjective Animal 
 ![](./a3.jpg)
 
-Cute slugs from the terminal in seconds. Random and reasonably unique human-readable ID strings using all the unique adjectives and animal names I could find online in 4 hours.
+Cute slugs from the terminal in mere moemnts. Kinda random and reasonably unique human-readable ID strings using all the unique adjectives and animal names I could find online in 4 hours. 
+
+This has been certified by NIST to be the cutest way to generate a unique ID.
 
 Greatly expands work done by a-type [here](https://github.com/a-type/adjective-adjective-animal).
 
@@ -14,7 +16,7 @@ Greatly expands work done by a-type [here](https://github.com/a-type/adjective-a
     $ yarn global add elliotberry/adjective-adjective-animal
     ```
 
-2. Scream in 2 da terminal:
+2. Scream in 2 the terminal:
 
    ```sh
     $ a3
--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 #!/usr/bin/env node
-import generate from './lib/lib.js';
+import { generate } from './lib/lib.js';
 import chalk from 'chalk';
-import format from './lib/format.js';
+import { format, describeAllOptions } from './lib/format.js';
 import yargs from 'yargs/yargs';
 
 const run = async () => {
   var argv = yargs(process.argv.slice(2))
     .usage('Usage: $0 [options]')
-    .alias('o', 'output')
-    .describe('o', 'output directory')
     .alias('f', 'format')
-    .describe('f', 'output format (string, camel, capital, constant, dot, header, no, param, pascal, path, sentence, snake)')
+    .describe('f', `output format type; options are:\n ${describeAllOptions()}`)
     .alias('a', 'adjectives')
     .describe('a', 'number of adjectives')
     .example([['$0 -f capital', 'create a random name in capital case']])
-    .version(process.env.npm_package_version).argv;
+    .version(process.env.npm_package_version)
+    .argv;
 
   let numberOfAdjectives = argv.adjectives || 1;
   let formatType = argv.format || 'string';
   formatType = formatType.toLowerCase();
+
   generate(numberOfAdjectives).then(function (output) {
     let formattedOutput = format(output, formatType);
     console.log(chalk.green.bold(formattedOutput));
   });
 };
+
 run();
