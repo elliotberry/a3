9d8cdc5d7d1d8d0090e3ad919136740e34b95f20
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-05-13 22:52:06 -0400

    updated readme, some package updates,updated eslint to modern config, then linted using said config

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# Adjective Adjective Animal 
+# a three
+> (adjective adjective animal)
+
 ![](./a3.jpg)
 
-Cute slugs from the terminal in mere moemnts. Kinda random and reasonably unique human-readable ID strings using all the unique adjectives and animal names I could find online in 4 hours. 
-
-This has been certified by NIST to be the cutest way to generate a unique ID.
-
-Greatly expands work done by a-type [here](https://github.com/a-type/adjective-adjective-animal).
-
+Mildly expands the awesome work done by Grant Forrest (a-type) [here](https://github.com/a-type/adjective-adjective-animal).
+### what is
+* Kinda random and reasonably unique human-readable ID strings using all the unique adjectives and animal names I could find online in 4 hours.
+* Cute slugs from the terminal in he blink of an eye, probably.
+* This has been certified by NIST to be the cutest way to generate a unique ID.
 
 ## Usagé
 
 1. Installation
 
     ```sh
-    $ yarn global add elliotberry/adjective-adjective-animal
+    yarn global add elliotberry/adjective-adjective-animal
     ```
 
 2. Scream in 2 the terminal:
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-import { generate } from './lib/lib.js';
 import chalk from 'chalk';
-import { format, describeAllOptions } from './lib/format.js';
 import yargs from 'yargs/yargs';
 
+import {describeAllOptions, format} from './lib/format.js';
+import {generate} from './lib/lib.js';
+
 const run = async () => {
-  var argv = yargs(process.argv.slice(2))
+  const argv = yargs(process.argv.slice(2))
     .usage('Usage: $0 [options]')
     .alias('f', 'format')
     .describe('f', `output format type; options are:\n ${describeAllOptions()}`)
     .alias('a', 'adjectives')
     .describe('a', 'number of adjectives')
     .example([['$0 -f capital', 'create a random name in capital case']])
-    .version(process.env.npm_package_version)
-    .argv;
+    .version(process.env.npm_package_version).argv;
 
-  let numberOfAdjectives = argv.adjectives || 1;
+  const numberOfAdjectives = argv.adjectives || 1;
   let formatType = argv.format || 'string';
   formatType = formatType.toLowerCase();
 
   generate(numberOfAdjectives).then(function (output) {
-    let formattedOutput = format(output, formatType);
+    const formattedOutput = format(output, formatType);
     console.log(chalk.green.bold(formattedOutput));
   });
 };
